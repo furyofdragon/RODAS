d0ea65a9225cbfb6f373c12edef3ab582a45bb3d
furyofdragon <furyofdragon@gmail.com>
2017-01-04 23:06:48 +0300

    work with main window

--- RODAS/src/main/Main.java
+++ RODAS/src/main/Main.java
@@ -4,10 +4,21 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.Frame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
 
 public class Main {
 
 	private JFrame mainWindow;
+	private JTextField statusBar;
 
 	/**
 	 * Launch the application.
@@ -45,6 +56,42 @@ public class Main {
 		mainWindow.setTitle("RODAS");
 		mainWindow.setBounds(startXPosition, startYPosition, with, height);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		mainWindow.setJMenuBar(menuBar);
+		
+		JMenu menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		
+		JMenuItem menuFileOpen = new JMenuItem("Open...");
+		menuFileOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		menuFile.add(menuFileOpen);
+		
+		JMenuItem menuFileSave = new JMenuItem("Save");
+		menuFileSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		menuFile.add(menuFileSave);
+		
+		JMenuItem menuFileSaveAs = new JMenuItem("Save As...");
+		menuFile.add(menuFileSaveAs);
+		
+		JMenu menuLF = new JMenu("L&F");
+		menuBar.add(menuLF);
+		
+		JMenu menuHelp = new JMenu("Help");
+		menuBar.add(menuHelp);
+		
+		JMenuItem menuHelpAbout = new JMenuItem("About");
+		menuHelp.add(menuHelpAbout);
+		
+		JPanel bottomPanel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) bottomPanel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		mainWindow.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+		
+		statusBar = new JTextField();
+		statusBar.setEditable(false);
+		statusBar.setColumns(30);
+		bottomPanel.add(statusBar);
 	}
 
 }
